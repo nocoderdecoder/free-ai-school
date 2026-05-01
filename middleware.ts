c7de6344e00d66d402ac8159f02428c6efb8e121
Anshul Gupta <anshul@nocoderdecoder.com>
2026-05-01 12:12:25 -0700

    fix cors and middleware

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher([
-  '/',
-  '/sign-in(.*)',
-  '/sign-up(.*)',
-  '/about(.*)',
-  '/lab(.*)',
-  '/learn(.*)',
-  '/writing(.*)',
-  '/studio(.*)',
+  '/(.*)',
 ])
 
 export default clerkMiddleware(async (auth, request) => {
@@ -18,8 +11,5 @@ export default clerkMiddleware(async (auth, request) => {
 })
 
 export const config = {
-  matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ['/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)', '/(api|trpc)(.*)'],
 }
